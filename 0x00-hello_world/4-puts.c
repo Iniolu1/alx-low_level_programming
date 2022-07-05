769702e320aa7316ca2bedf735de2ff9759b9541
Inioluwa Onabowale <inioluwaonabowale52@gmail.com>
2022-07-05 11:35:26 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* main: exactly Programming is like building a multilingual puzzle, followed by a new line
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle\n");
+    return(0);
+}    
